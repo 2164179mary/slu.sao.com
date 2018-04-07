c2d427855d334ab0f923ca177f51559bd27dce38
Mary <Mary@gmail.com>
2018-04-08 06:11:30 +0800

    initial

---
+++ PWA/scripts/app.js
@@ -0,0 +1,16 @@
+(function) {
+	'use strict';
+}
+var app = {
+	isLoading: true,
+	spinner: document.querySelector('.loader')
+}
+/**
+ *Events Listener 
+**/
+function load(http://127.0.0.1:8887/){
+	document.getElementById('home-logo').visible = true;
+	var req = new XMLHttpRequest();
+	req.open ("POST", http://127.0.0.1:8887/, true);
+}
+
